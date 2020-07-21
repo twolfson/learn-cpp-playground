66ecf7edd6240b687bd911c4638b23cef59141af
Todd Wolfson <todd@twolfson.com>
2020-07-21 15:16:33 -0700

    Improved directory navigation for build script

--- 00-program-structure/build.sh
+++ 00-program-structure/build.sh
@@ -2,5 +2,8 @@
 # Exit on first error, unset variable, or pipe failure
 set -euo pipefail
 
+# Navigate to same folder as compilation files
+cd "$(dirname ${BASH_SOURCE[0]})"
+
 # Build our program
 g++ main.cpp -o main.out
