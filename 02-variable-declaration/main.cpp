61ef7a4e2bb5ef38ae3ea8a2d22e28b4d598f662
Todd Wolfson <todd@twolfson.com>
2020-07-28 23:03:21 -0700

    Continued learning variable declaration

---
+++ 02-variable-declaration/main.cpp
@@ -0,0 +1,22 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Declare some variables
+  int a, b;
+  int result;
+
+  // Process our variables
+  a = 5;
+  b = 2;
+  a += 1;
+  result = a - b;
+
+  // Output our variables
+  cout << result;
+  cout << "\n";
+
+  return 0;
+}
