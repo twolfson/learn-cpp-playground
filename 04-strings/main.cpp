41fe445dcdcc964066d56b0203058e5a23860672
Todd Wolfson <todd@twolfson.com>
2020-07-28 23:25:06 -0700

    Started using C++ strings

---
+++ 04-strings/main.cpp
@@ -0,0 +1,14 @@
+// my first program in C++
+#include <iostream>
+
+int main()
+{
+  // Declare and initialize our variables
+  // DEV: Close spacing seems to be a thing for initialization
+  std::string mystring = "Hello World!";
+
+  // Output our variables
+  std::cout << mystring << std::endl;
+
+  return 0;
+}
