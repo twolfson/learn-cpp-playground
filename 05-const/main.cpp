836f43a54f53d8144a28ec8abfc36dbd7af6a589
Todd Wolfson <todd@twolfson.com>
2020-08-18 16:14:02 -0700

    Added const exploration

---
+++ 05-const/main.cpp
@@ -0,0 +1,17 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+const double pi = 3.14159;
+
+int main()
+{
+  // Attempt to override `pi`
+  pi = 3.14;
+
+  // Output pi
+  cout << pi << endl;
+
+  // Return 0 as desired
+  return 0;
+}
