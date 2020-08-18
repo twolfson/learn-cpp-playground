446b360ec348bfed2256ff0cf638423a5feafdb1
Todd Wolfson <todd@twolfson.com>
2020-08-18 16:54:16 -0700

    Added missed #define example

---
+++ 06-define/main.cpp
@@ -0,0 +1,15 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+#define PI 3.14159
+#define PI 3
+
+int main()
+{
+  // Output pi
+  cout << PI << endl;
+
+  // Return 0 as desired
+  return 0;
+}
