ffda06aee6d4f6331d587ac0ab9dafc4fae60580
Todd Wolfson <todd@twolfson.com>
2020-08-19 01:57:03 -0700

    Extended our pass-by-reference exploration with strings

--- 09-pass-by-reference/main.cpp
+++ 09-pass-by-reference/main.cpp
@@ -6,10 +6,16 @@ int increase(int& a) {
   a *= 2;
 }
 
+int extend(string& a) {
+  a += "bar";
+}
+
 int main()
 {
   // Call our function and output the result
-  int x = 2;
-  increase(x);
+  // int x = 2;
+  // increase(x);
+  string x = "foo";
+  extend(x);
   cout << x << endl;
 }
