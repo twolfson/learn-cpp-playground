1dc7211503b89302ff42447bebc9926a4d9a3286
Todd Wolfson <todd@twolfson.com>
2020-08-19 15:15:23 -0700

    Learned about inline functions and default values

---
+++ 10-default-values/main.cpp
@@ -0,0 +1,21 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+// string concat(string& a, string b="hi") {
+string concat(string& a, string& b) {
+  return a+b;
+}
+string concat(string& a) {
+  string b = "hi";
+  return concat(a, b);
+}
+
+int main()
+{
+  // Call our function and output the result
+  string x = "foo";
+  string y = "bar";
+  string output = concat(x);
+  cout << output << endl;
+}
