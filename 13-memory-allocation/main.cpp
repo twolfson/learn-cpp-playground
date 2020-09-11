df1ac38c507c2d1832bf7d805e8b02030d850941
Todd Wolfson <todd@twolfson.com>
2020-09-11 03:26:41 -0700

    Started learning about dynamic memory

---
+++ 13-memory-allocation/main.cpp
@@ -0,0 +1,9 @@
+// Load in our dependencies
+#include <iostream>
+using namespace std;
+
+int main() {
+  int* num_arr;
+  unsigned long num_arr_length = 1000000000000000;
+  num_arr = new int [num_arr_length];
+}
