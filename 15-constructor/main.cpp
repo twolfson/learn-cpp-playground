140299e0e5a0e6ef96905a42519128bac944b671
Todd Wolfson <todd@twolfson.com>
2020-09-14 04:20:30 -0700

    Rolled back 15-constructor changes to original inspection

--- 15-constructor/main.cpp
+++ 15-constructor/main.cpp
@@ -5,14 +5,15 @@ using namespace std;
 class Rectangle {
   public:
     int width, height;
-    Rectangle ();
+  private:
+    Rectangle (int, int);
 };
-Rectangle::Rectangle () {
-  width = 5;
-  height = 5;
+Rectangle::Rectangle (int a, int b) {
+  width = a;
+  height = b;
 }
 
 int main (){
-  Rectangle rect_a();
+  Rectangle rect_a(3, 4);
   cout << rect_a.width << endl;
 }
