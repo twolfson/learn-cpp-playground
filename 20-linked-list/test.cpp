ac690ea7cdd47ab2139e14cd05c04b73cd535397
Todd Wolfson <todd@twolfson.com>
2020-09-17 03:33:43 -0700

    Completed remainder of test suite

--- 20-linked-list/test.cpp
+++ 20-linked-list/test.cpp
@@ -19,32 +19,27 @@ Describe(A_linked_list_node_with_1_element) {
     Assert::That(head_node.value, Equals(100));
   };
 
-  // It(has_no_next_node) {
-  //   Assert::That(head_node.next(), Equals(NULL));
-  // };
+  It(has_no_next_node) {
+    Assert::That(head_node.next, Equals(nullptr));
+  };
 };
 
-// Describe(A_linked_list_node_with_2_elements) {
-//   Node head_node {100};
-//   Node tail_node {200};
-//   void SetUp() {
-//     head_node.set_next(tail_node);
-//   }
-//   // Node head_node;
-//   // Node tail_node;
-//   // void SetUp() {
-//   //   head_node = Node {100};
-//   //   tail_node = Node {200};
-//   //   head_node.set_next(tail_node);
-//   // }
+Describe(A_linked_list_node_with_2_elements) {
+  Node head_node;
+  Node tail_node;
+  void SetUp() {
+    head_node = Node {100};
+    tail_node = Node {200};
+    head_node.set_next(tail_node);
+  }
 
-//   It(allows_retrieving_tail_node_from_head_node) {
-//     Assert::That(head_node.next(), Equals(tail_node));
-//   };
+  It(allows_retrieving_tail_node_from_head_node) {
+    Assert::That(head_node.next, Equals(&tail_node));
+  };
 
-//   // DEV: We could build out functionality like an iterator across all linked list nodes
-//   //   e.g. with an actual LinkedList class but this is prob a good enough start, right?
-// };
+  // DEV: We could build out functionality like an iterator across all linked list nodes
+  //   e.g. with an actual LinkedList class but this is prob a good enough start, right?
+};
 
 // Run our tests when being run directly
 int main(int argc, /* const */ char *argv[]) {
