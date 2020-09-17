7fc820c1fcfd2056152442ac17797caa259ced1d
Todd Wolfson <todd@twolfson.com>
2020-09-17 04:25:07 -0700

    Implemented destructor

--- 20-linked-list/main.cpp
+++ 20-linked-list/main.cpp
@@ -1,13 +1,17 @@
 // Load in our headers
 #include "./main.hpp"
 
-// Define our class
+// Define our class and its implementation
 namespace playground {
-  // Define our constructor and methods
   Node::Node(int _value) {
     this->value = _value;
     this->next = nullptr;
   };
+
+  Node::~Node() {
+    this->value = 0;
+    this->next = nullptr;
+  }
 
   Node* Node::set_next(Node* node_ptr) {
     this->next = node_ptr;
