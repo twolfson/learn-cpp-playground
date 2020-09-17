b896d7a82b02cd2fe0373cba3c95ff3a3e36e479
Todd Wolfson <todd@twolfson.com>
2020-09-17 03:52:19 -0700

    Revert "Built our common _init constructor for sake of it"
    This reverts commit 1754d8ab5cc6c4ee51756d7c083eb3686f074550.

--- 20-linked-list/main.cpp
+++ 20-linked-list/main.cpp
@@ -4,9 +4,8 @@ namespace playground {
       int value;
       Node* next;
 
-      Node();
       Node(int _value);
-      void _init(int _value);
+      Node();
       Node set_next(Node& node);
       // TODO: Build out teardown and other constructors for practice
   };
@@ -14,13 +13,8 @@ namespace playground {
   // Define our constructor and methods
   // DEV: We alias `Node::Node()` (required for declaration -- though looking into alternatives)
   //   https://stackoverflow.com/a/308318
-  Node::Node() {
-    this->_init(0);
-  }
+  Node::Node() : Node::Node(0) {};
   Node::Node(int _value) {
-    this->_init(_value);
-  };
-  void Node::_init(int _value) {
     this->value = _value;
     this->next = nullptr;
   };
