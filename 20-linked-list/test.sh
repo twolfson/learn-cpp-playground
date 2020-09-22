19dcd728a1167125b7d2cbc73adffaf871b7434f
Todd Wolfson <todd@twolfson.com>
2020-09-21 19:56:19 -0700

    Completed blazing fast builds

--- 20-linked-list/test.sh
+++ 20-linked-list/test.sh
@@ -9,10 +9,9 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 # DEV: We need to specify `igloo` in our CLI path to allow its `<>` import style
 # DEV: By using `-isystem` instead of `-I`, https://stackoverflow.com/a/1900578
 # TODO: By using `.` as a system dependency, we might be missing out on warnings
-mkdir -p build
-ccache g++ -c test-files/*.cpp
-ccache g++ -c -isystem . test.cpp
-ccache g++ build/test.o build/test-files.o -o test.out
+ccache g++ -isystem . -c test.cpp
+ccache g++ -isystem . -c test-files/*.cpp
+ccache g++ *.o -o test.out
 
 # Run our program
 # DEV: We could make this optional but meh
