180041d233de9382ffc5eff885f0ce870f9a1500
Todd Wolfson <todd@twolfson.com>
2020-09-23 03:41:12 -0700

    Exploring adding better linting

--- 20-linked-list/test.sh
+++ 20-linked-list/test.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 # Navigate to same folder as compilation files
 cd "$(dirname ${BASH_SOURCE[0]})"
 
+# Define a common compiler program/settings
+compiler="ccache g++ -isystem ."
+
 # Build our program
 # DEV: We need to specify `igloo` in our CLI path to allow its `<>` import style
 # DEV: By using `-isystem` instead of `-I`, https://stackoverflow.com/a/1900578
@@ -14,12 +17,19 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 rm -r build &> /dev/null || true
 mkdir -p build
 mkdir -p build/test-files
-ccache g++ -isystem . -c test.cpp -o build/test.cpp.o
+$compiler -c test.cpp -o build/test.cpp.o
 for filepath in test-files/*.cpp; do
-  ccache g++ -c "${filepath}" -o build/"${filepath}".o
+  $compiler -c "${filepath}" -o build/"${filepath}".o
 done
-ccache g++ build/*.o build/test-files/*.o -o test.out
+$compiler build/*.o build/test-files/*.o -o test.out
 
 # Run our program
 # DEV: We could make this optional but meh
 ./test.out
+
+# Lint our files
+lint_output=$($compiler -Wall -Wextra -Wpedantic *.cpp test-files/*.cpp -o /dev/null 2>&1)
+if test "$lint_output" != ""; then
+  echo "$lint_output" 1>&2
+  exit 1
+fi
