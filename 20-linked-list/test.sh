b6682e635a12eed74c99d33132288bbe42bf3b97
Todd Wolfson <todd@twolfson.com>
2020-09-23 03:47:32 -0700

    Fixed up linting to our wants

--- 20-linked-list/test.sh
+++ 20-linked-list/test.sh
@@ -14,22 +14,25 @@ compiler="ccache g++ -isystem ."
 # TODO: By using `.` as a system dependency, we might be missing out on warnings
 # DEV: By splitting up `.cpp` builds, we can use `ccache` and `-c` to cache each compilation
 #   So far: 1s -> 0.1s
-rm -r build &> /dev/null || true
-mkdir -p build
-mkdir -p build/test-files
-$compiler -c test.cpp -o build/test.cpp.o
-for filepath in test-files/*.cpp; do
-  $compiler -c "${filepath}" -o build/"${filepath}".o
-done
-$compiler build/*.o build/test-files/*.o -o test.out
+build_test() {
+  rm -r build &> /dev/null || true
+  mkdir -p build
+  mkdir -p build/test-files
+  ${compiler} -c test.cpp -o build/test.cpp.o
+  for filepath in test-files/*.cpp; do
+    ${compiler} -c "${filepath}" -o build/"${filepath}".o
+  done
+  ${compiler} build/*.o build/test-files/*.o -o test.out
+}
+build_test
 
 # Run our program
 # DEV: We could make this optional but meh
 ./test.out
 
-# Lint our files
-lint_output=$($compiler -Wall -Wextra -Wpedantic *.cpp test-files/*.cpp -o /dev/null 2>&1)
-if test "$lint_output" != ""; then
-  echo "$lint_output" 1>&2
-  exit 1
-fi
+# Rebuild out files with lint errors, utilizing caching
+# DEV: `-Wall/extra/pedantic` enable errors
+#   `-Werror` upcasts warnings to errors for exit code
+#   `-fdiagnostics` ensures we get color in our output
+compiler="${compiler} -Wall -Wextra -Wpedantic -Werror -fdiagnostics-color=always"
+build_test
